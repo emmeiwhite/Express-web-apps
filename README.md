425d62fac0cb65372e6a8d6945a703c590db6ef1
emmeiwhite <emmeiwhite@gmail.com>
2024-11-08 19:52:27 +0530

    Exp-1.3 Added README.md file, & wrote about REST Architectural Style

---
+++ README.md
@@ -0,0 +1,13 @@
+# First Chapter
+
+## REST Architectural Style
+
+Rest is an Architectural Style defined to help create & organize Distributed Systems.
+
+Roy Fielding, an American Computer Scientist born in 1965, proposed this architectural style to interconnect distributed systems (nonhomogeneous set of systems).
+
+Roy is one of the main authors of the HTTP protocol (the protocol upon which the entire web infrastructure is based).
+
+In 2000, Fielding presented his doctoral dissertation, _Architectural Styles & the Design of Network-based Software Architecture_. In it, he coined the term REST, an architectural style for distributed hypermedia systems.
+
+Put simply, REST (Representational State Transfer) is the Architectural Style to help create & organize distributed systems. The key word from this definition should be _style_, because an important aspect of REST is that it is an Architectural Style, not a guideline, protocol, standard, or anything that would imply there are hard rules to follow to achieve a RESTful Architecture.
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-console.log('Nodemon Installed! We are good to go!')
-console.log('Architectural Solution!')
-console.log('Web Architecture based on REST style rules, is called a RESTful architecture')
-console.log('REST is an architectural style defined to help create & organize distributed System')
 // Rest is an Architectural Style defined to help create & organize Distributed Systems
+
+// Roy Fielding an American Computer Scientist born in 1965 proposed architectural style to interconnect distributed systems (nonhomogeneous set of systems)
+
+// Roy is one of the main authors of the HTTP protocol (the protocol upon which the entire web infrastructure is based).
+
+// In the 2000, Fielding presented his doctoral dissertation, Architectural Styles & the Design of Network-based Software Architecture. In it he coined the term REST, an architectural style for distributed hypermedia systems.
+
+// Put simply, REST(Representational State Transfer) is the Architectural Style to help create & organize distributed systems. The key word from this definition should be style, because an important aspect of REST is that it is an Architectural Style, not a guideline, not a protocol, not a standard, nor anything that would imply that there are a set of hard rules to follow to end up having a RESTful Architecture
