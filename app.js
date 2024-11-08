dae185145dad7e174c70be5b9c2f511fb173c864
emmeiwhite <emmeiwhite@gmail.com>
2024-11-08 19:20:56 +0530

    Exp-1.2 REST Architectural Style for Distributed Web Systems

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log('Nodemon Installed! We are good to go!')
+console.log('Architectural Solution!')
+console.log('Web Architecture based on REST style rules, is called a RESTful architecture')
+console.log('REST is an architectural style defined to help create & organize distributed System')
+// Rest is an Architectural Style defined to help create & organize Distributed Systems
