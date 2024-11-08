82329518eb3245816f36117334383c0c0304ff3e
emmeiwhite <emmeiwhite@gmail.com>
2024-11-08 20:00:19 +0530

    Exp-1.5 Serving Homepage for main route for the request coming from client

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 const express = require('express')
 const app = express()
 
+// First Route
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Hello World!</h1>')
+})
+
 app.listen(3300, () => {
   console.log('The app is listening on port 3300')
 })
